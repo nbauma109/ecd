44496fa80ae6814702515f7cfa8f3009dd5b8e51
nbauma109 <nbauma109@github.com>
2022-12-07 15:09:55 +0100

    get latest maven version

--- jitci-build.sh
+++ jitci-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-export M3_VERSION=3.8.6
+export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
 export JAVA_HOME=$HOME/jdk-17.0.2+8
 $HOME/apache-maven-${M3_VERSION}/bin/mvn -B install -DskipTests
