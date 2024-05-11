7af86bebd9d639509aa24da6ef6a5158dba0fec4
nbauma109 <nbauma109@github.com>
2024-05-11 21:37:53 +0200

    fix jitci-init.sh

--- jitci-init.sh
+++ jitci-init.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 . ~/.sdkman/bin/sdkman-init.sh
-sdk selfupdate force
-sdk list java
-ls -lrt ~/.sdkman/candidates/java/
 sdk install java 17.0.10-sem
 sdk use java 17.0.10-sem
-ls -lrt ~/.sdkman/candidates/java/
-java -version
 export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
 mvn wrapper:wrapper -Dmaven=${M3_VERSION} --no-transfer-progress
 ./mvnw -B de.qaware.maven:go-offline-maven-plugin:resolve-dependencies --no-transfer-progress
