ee6492c136fa1bfa7ec4483bf73159d5df5ef7b3
nbauma109 <nbauma109@github.com>
2024-05-11 20:13:04 +0200

    use sdkman to install java

--- jitci-init.sh
+++ jitci-init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-sdk install java 17.0.11-oracle
-sdk use java 17.0.11-oracle
+bash ensure-java-17-sem install
+if ! bash ensure-java-17-sem use; then source ~/.sdkman/bin/sdkman-init.sh; fi
+java -version
 export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
 mvn wrapper:wrapper -Dmaven=${M3_VERSION} --no-transfer-progress
 ./mvnw -B de.qaware.maven:go-offline-maven-plugin:resolve-dependencies --no-transfer-progress
