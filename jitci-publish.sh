53f31a06a3dd2bd9e973fb18694c4769fac2eab7
nbauma109 <nbauma109@github.com>
2024-05-11 19:33:19 +0200

    use sdkman to install java

--- jitci-publish.sh
+++ jitci-publish.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
-mvn wrapper:wrapper -Dmaven=${M3_VERSION} --no-transfer-progress
 ./mvnw -B deploy -DskipTests -Dfindbugs.skip=true -Dpmd.skip=true -Dcheckstyle.skip=true -Dsigning.disabled=true -DaltDeploymentRepository=jitci::default::file:///home/jitpack/deploy --no-transfer-progress
