a93416ec0425b30f4f15fbaf91574cee51f3ae41
nbauma109 <nbauma109@github.com>
2024-05-11 20:42:02 +0200

    fix jitci-init.sh

--- jitci-init.sh
+++ jitci-init.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 . ~/.sdkman/bin/sdkman-init.sh
 sdk selfupdate force
-sdk list java
-sdk install java 17.0.10-sem
-sdk use 17.0.10-sem
+sdk use java 17.0.10-sem
 java -version
 export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
 mvn wrapper:wrapper -Dmaven=${M3_VERSION} --no-transfer-progress
