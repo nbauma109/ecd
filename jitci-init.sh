6de5831a286b1fb00f8079dbe31c92d959c9c66e
nbauma109 <nbauma109@github.com>
2024-05-11 20:32:52 +0200

    fix jitci-init.sh

--- jitci-init.sh
+++ jitci-init.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-. ~/.sdkman/bin/sdkman-init.sh
-sdk selfupdate force
-sdk list java
+bash ensure-java-17-sem install
+if ! bash ensure-java-17-sem use; then source ~/.sdkman/bin/sdkman-init.sh; fi
 java -version
 export M3_VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/apache/maven/releases/latest | sed 's,https://github.com/apache/maven/releases/tag/maven-,,g')
 mvn wrapper:wrapper -Dmaven=${M3_VERSION} --no-transfer-progress
