96894762be72fd1c9e4e65ce02c463f55460228e
nbauma109 <nbauma109@users.noreply.github.com>
2025-09-04 22:07:04 +0200

    Update jitci-build.sh

--- jitci-build.sh
+++ jitci-build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-export JAVA_HOME=~/.sdkman/candidates/java/current
-./mvnw -B install -DskipTests -Dsigning.disabled=true --no-transfer-progress
+set -euo pipefail
+
+export JAVA_HOME="$HOME/.sdkman/candidates/java/current"
+
+mvn -B install -DskipTests -Dsigning.disabled=true --no-transfer-progress
