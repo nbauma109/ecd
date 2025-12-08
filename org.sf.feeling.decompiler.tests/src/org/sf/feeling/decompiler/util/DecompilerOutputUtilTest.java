9d6d15a7fd25dbee72371be5596c09dc4671edb5
nbauma109 <nbauma109@github.com>
2025-12-08 22:20:00 +0100

    refactor tests

--- org.sf.feeling.decompiler.tests/src/org/sf/feeling/decompiler/util/DecompilerOutputUtilTest.java
+++ org.sf.feeling.decompiler.tests/src/org/sf/feeling/decompiler/util/DecompilerOutputUtilTest.java
@@ -1,284 +1,72 @@
 package org.sf.feeling.decompiler.util;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.sf.feeling.decompiler.util.TextAssert.assertEquivalent;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 public class DecompilerOutputUtilTest {
 
     @Test
-    public void testToStringExampleOneFernFlower() {
-        DecompilerOutputUtil decompilerOutputUtil = new DecompilerOutputUtil("""
-package sample.one;
+    public void testToStringPackageImportFernflower() throws IOException, URISyntaxException {
+        test("PackageImportFernflower");
+    }
 
-import java.util.Arrays;
-
-public class ExampleOne {
-   public void run() {
-      for (String value : Arrays.asList("a", "b")) {// 8 9
-         System.out.println(value);// 10
-      }
-   }// 12
-}
-""");
-        decompilerOutputUtil.realign();
-        String output = decompilerOutputUtil.toString();
-        assertEquivalent("""
-/*    */ package sample.one;
-/*    */
-/*    */ import java.util.Arrays;
-/*    */
-/*    */
-/*    */ public class ExampleOne {
-/*    */    public void run() {
-/*  8 */       for (String value : Arrays.asList("a", "b")) {
-/*    */
-/* 10 */          System.out.println(value);
-/*    */       }
-/*    */    }
-/*    */ }
-""", output);
+    @Test
+    public void testToStringPackageNoImportFernflower() throws IOException, URISyntaxException {
+        test("PackageNoImportFernflower");
     }
     
     @Test
-    public void testToStringExampleTwoFernFlower() {
-        DecompilerOutputUtil decompilerOutputUtil = new DecompilerOutputUtil("""
-package sample.two;
-
-public class ExampleTwo {
-   public void run() {
-      System.out.println("Hello");// 6
-   }// 7
-}
-""");
-        decompilerOutputUtil.realign();
-        String output = decompilerOutputUtil.toString();
-        assertEquivalent("""
-/*   */ package sample.two;
-/*   */
-/*   */
-/*   */ public class ExampleTwo {
-/*   */    public void run() {
-/* 6 */       System.out.println("Hello");
-/*   */    }
-/*   */ }
-""", output);
+    public void testToStringImportNoPackageFernflower() throws IOException, URISyntaxException {
+        test("ImportNoPackageFernflower");
     }
     
     @Test
-    public void testToStringExampleThreeFernFlower() {
-        DecompilerOutputUtil decompilerOutputUtil = new DecompilerOutputUtil("""
-import java.util.*;
+    public void testToStringNoPackageNoImportFernflower() throws IOException, URISyntaxException {
+        test("NoPackageNoImportFernflower");
+    }
 
-public class ExampleThree {
-   public void run() {
-      Set<String> values = new HashSet();// 6
-      values.add("x");// 7
-      values.add("y");// 8
-
-      for (String value : values) {// 9
-         System.out.println(value);// 10
-      }
-   }// 12
-}
-""");
-        decompilerOutputUtil.realign();
-        String output = decompilerOutputUtil.toString();
-        assertEquivalent("""
-/*    */ import java.util.*;
-/*    */
-/*    */
-/*    */ public class ExampleThree {
-/*    */    public void run() {
-/*  6 */       Set<String> values = new HashSet();
-/*  7 */       values.add("x");
-/*  8 */       values.add("y");
-/*  9 */       for (String value : values) {
-/* 10 */          System.out.println(value);
-/*    */       }
-/*    */    }
-/*    */ }
-""", output);
+    @Test
+    public void testToStringPackageImportProcyon() throws IOException, URISyntaxException {
+        test("PackageImportProcyon");
     }
     
     @Test
-    public void testToStringExampleFourFernFlower() {
-        DecompilerOutputUtil decompilerOutputUtil = new DecompilerOutputUtil("""
-
-public class ExampleFour {
-   public void run() {
-      System.out.println("No package and no import");// 4
-   }// 5
-}
-""");
-        decompilerOutputUtil.realign();
-        String output = decompilerOutputUtil.toString();
-        assertEquivalent("""
-/*   */
-/*   */ public class ExampleFour {
-/*   */    public void run() {
-/* 4 */       System.out.println("No package and no import");
-/*   */    }
-/*   */ }
-""", output);
-    }
-
-    @Test
-    public void testToStringExampleOneProcyon() {
-        DecompilerOutputUtil decompilerOutputUtil = new DecompilerOutputUtil("""
-      package sample.one;
-      
-      import java.util.*;
-      
-      public class ExampleOne
-      {
-          public void run() {
-/* 8*/        final List<String> data = Arrays.asList("a", "b");
-/* 9*/        for (final String value : data) {
-/*10*/            System.out.println(value);
-              }
-          }
-      }
-""");
-        decompilerOutputUtil.realign();
-        String output = decompilerOutputUtil.toString();
-        assertEquivalent("""
-/*    */ package sample.one;
-/*    */
-/*    */ import java.util.*;
-/*    */
-/*    */ public class ExampleOne
-/*    */ {
-/*    */     public void run() {
-/*  8 */         final List<String> data = Arrays.asList("a", "b");
-/*  9 */         for (final String value : data) {
-/* 10 */             System.out.println(value);
-/*    */         }
-/*    */     }
-/*    */ }
-""", output);
+    public void testToStringPackageNoImportProcyon() throws IOException, URISyntaxException {
+        test("PackageNoImportProcyon");
     }
     
     @Test
-    public void testToStringExampleTwoProcyon() {
-        DecompilerOutputUtil decompilerOutputUtil = new DecompilerOutputUtil("""
-     package sample.two;
-     
-     public class ExampleTwo
-     {
-         public void run() {
-/*6*/        System.out.println("Hello");
-         }
-     }
-""");
-        decompilerOutputUtil.realign();
-        String output = decompilerOutputUtil.toString();
-        assertEquivalent("""
-/*   */ package sample.two;
-/*   */
-/*   */ public class ExampleTwo
-/*   */ {
-/*   */     public void run() {
-/* 6 */         System.out.println("Hello");
-/*   */     }
-/*   */ }
-""", output);
+    public void testToStringImportNoPackageProcyon() throws IOException, URISyntaxException {
+        test("ImportNoPackageProcyon");
     }
     
     @Test
-    public void testToStringExampleThreeProcyon() {
-        DecompilerOutputUtil decompilerOutputUtil = new DecompilerOutputUtil("""
-      import java.util.*;
-      
-      public class ExampleThree
-      {
-          public void run() {
-/* 6*/        final Set<String> values = new HashSet<String>();
-/* 7*/        values.add("x");
-/* 8*/        values.add("y");
-/* 9*/        for (final String value : values) {
-/*10*/            System.out.println(value);
-              }
-          }
-      }
-""");
+    public void testToStringNoPackageNoImportProcyon() throws IOException, URISyntaxException {
+        test("NoPackageNoImportProcyon");
+    }
+
+    @Test
+    public void testTryCatchFernflower() throws IOException, URISyntaxException {
+        test("TryCatchFernflower");
+    }
+
+    private void test(String testName) throws IOException, URISyntaxException {
+        String input = toString(getClass().getResource("/input/" + testName + ".txt"));
+        String expected = toString(getClass().getResource("/output/" + testName + ".txt"));
+        DecompilerOutputUtil decompilerOutputUtil = new DecompilerOutputUtil(input);
         decompilerOutputUtil.realign();
         String output = decompilerOutputUtil.toString();
-        assertEquivalent("""
-/*    */ import java.util.*;
-/*    */
-/*    */ public class ExampleThree
-/*    */ {
-/*    */     public void run() {
-/*  6 */         final Set<String> values = new HashSet<String>();
-/*  7 */         values.add("x");
-/*  8 */         values.add("y");
-/*  9 */         for (final String value : values) {
-/* 10 */             System.out.println(value);
-/*    */         }
-/*    */     }
-/*    */ }
-""", output);
+        assertEquivalent(expected, output);
     }
     
-    @Test
-    public void testToStringExampleFourProcyon() {
-        DecompilerOutputUtil decompilerOutputUtil = new DecompilerOutputUtil("""
-     public class ExampleFour
-     {
-         public void run() {
-/*4*/        System.out.println("No package and no import");
-         }
-     }
-""");
-        decompilerOutputUtil.realign();
-        String output = decompilerOutputUtil.toString();
-        assertEquivalent("""
-
-/*   */ public class ExampleFour{
-/*   */     public void run() {
-/* 4 */         System.out.println("No package and no import");
-/*   */     }
-/*   */ }
-""", output);
+    private String toString(URL resource) throws IOException, URISyntaxException {
+        return IOUtils.toString(resource.toURI(), UTF_8);
     }
-
-    @Test
-    public void testTryCatchFernFlower() {
-        DecompilerOutputUtil util = new DecompilerOutputUtil("""
-package test;
-
-public class ExampleTry {
-  public static void main(String[] args) {
-    try {
-      Thread.sleep(100L);// 7
-    } catch (InterruptedException e) {// 8
-      e.printStackTrace();// 9
-    }
-
-  }// 11
-}
-"""
-        );
-
-        util.realign();
-        String output = util.toString();
-
-        assertEquivalent("""
-/*    */ package test;
-/*    */
-/*    */
-/*    */ public class ExampleTry {
-/*    */   public static void main(String[] args) {
-/*    */     try {
-/*  7 */       Thread.sleep(100L);
-/*  8 */     } catch (InterruptedException e) {
-/*  9 */       e.printStackTrace();
-/*    */     }
-/*    */
-/*    */   }
-/*    */ }
-""", output);
-    }
-
 }
