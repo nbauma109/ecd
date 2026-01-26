25475afcb7adb282c81f996e8c998db774cb7f53
nbauma109 <nbauma109@github.com>
2026-01-26 19:35:19 +0100

    build.bat: -UseBasicParsing

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ REM Usage: build.bat
 REM Example: build.bat
 
 :: Use PowerShell to get the latest Maven version by fetching the redirect URL and extracting the version number.
-for /f "delims=" %%i in ('powershell -Command "(Invoke-WebRequest -Uri https://github.com/apache/maven/releases/latest -MaximumRedirection 0 -ErrorAction SilentlyContinue).Headers.Location -replace '^.*/maven-', ''"') do set M3_VERSION=%%i
+for /f "delims=" %%i in ('powershell -Command "(Invoke-WebRequest -Uri https://github.com/apache/maven/releases/latest -UseBasicParsing -MaximumRedirection 0 -ErrorAction SilentlyContinue).Headers.Location -replace '^.*/maven-', ''"') do set M3_VERSION=%%i
 
 call mvn wrapper:wrapper -Dmaven=%M3_VERSION% --no-transfer-progress
 call mvnw.cmd clean verify -Dsigning.disabled=true --no-transfer-progress
