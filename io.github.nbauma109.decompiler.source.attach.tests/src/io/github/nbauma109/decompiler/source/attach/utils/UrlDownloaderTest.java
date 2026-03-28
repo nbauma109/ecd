11b9fbf7244ab0bc6088a324d6cb28f5cdab7d2d
Nicolas Baumann <nbauma109@users.noreply.github.com>
2026-03-28 23:13:53 +0100

    Apply Eclipse cleanup refactoring (#352)

--- io.github.nbauma109.decompiler.source.attach.tests/src/io/github/nbauma109/decompiler/source/attach/utils/UrlDownloaderTest.java
+++ io.github.nbauma109.decompiler.source.attach.tests/src/io/github/nbauma109/decompiler/source/attach/utils/UrlDownloaderTest.java
@@ -15,7 +15,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.UUID;
