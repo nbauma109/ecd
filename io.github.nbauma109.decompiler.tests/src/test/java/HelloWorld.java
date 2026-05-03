3a30fc6b8947e9479b693559a0ef8b3bb5f0d8f1
nbauma109 <nbauma109@github.com>
2026-05-03 18:20:43 +0200

    remove copyright notice in HelloWorld

--- io.github.nbauma109.decompiler.tests/src/test/java/HelloWorld.java
+++ io.github.nbauma109.decompiler.tests/src/test/java/HelloWorld.java
@@ -1,10 +1,3 @@
-/*******************************************************************************
- * © 2026 Nicolas Baumann (@nbauma109)
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
